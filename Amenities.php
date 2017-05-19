dc28a8d44232f4e3af1a8f3fd6f989da3414cc0d
mohammedomar <mohammedomar@10.20.13.119>
2017-05-19 14:59:19 +0400

    Project Creation

--- Amenities.php
+++ Amenities.php
@@ -57,10 +57,11 @@ if (!isset($_SESSION['loggedin']))
                                 <tr>
                                     <th>Amenity en name</th>
                                     <th>Amenity ar name</th>
-                                   
+                                    <th>Edit/Delete</th>
+
                                 </tr>
                             </thead>
-                                                    </table>
+                        </table>
 
                     </div>
                 </div>
@@ -95,6 +96,36 @@ if (!isset($_SESSION['loggedin']))
             </div>
 
         </div>
+                            <div class="modal inmodal" id="myModal" tabindex="-1" role="dialog" aria-hidden="true">
+                          <div class="modal-dialog">
+                              <div class="modal-content animated bounceInRight">
+                                  <div class="modal-body">
+                                          <form method="post" action="api/addAmenity.php" class="form-horizontal ">
+
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">English Name</label>
+                                <div class="col-sm-10"><input required="" name="amenityEn" placeholder="e.g., Gym" type="text" class="form-control"></div>
+
+                            </div>
+
+                            <div class="hr-line-dashed"></div>
+
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">Arabic Name</label>
+                                <div class="col-sm-10"><input required="" pattern="^[\u0621-\u064A0-9 ]+$"  name="amenityAr" placeholder="e.g., نادي رياضي" type="text" class="form-control"></div>
+
+                            </div>
+                            <div class="hr-line-dashed"></div>
+                            <div class="form-group">
+                                <div class="col-sm-4 col-sm-offset-2">
+                                    <button class="btn btn-primary" type="submit">Add amenity</button>
+                                </div>
+                            </div>
+                        </form>
+                                  </div>
+                              </div>
+                          </div>
+                            </div>
 
 
 
@@ -124,118 +155,25 @@ if (!isset($_SESSION['loggedin']))
 
         <script>
 
-         $(document).ready(function() {
-    $('#amenityTable').DataTable( {
-        
-        "ajax": "api/getAmenities.php",
-        "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]]
-      
-    } );
-} );
+            $(document).ready(function () {
+                $('#amenityTable').DataTable({
+
+                    "ajax": "api/getAmenities.php",
+                    "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
+                    "columnDefs": [{
+                            "targets": 2,
+                            "width":"10%",
+                            
+                            "render": function (data, type, row) {
+                                return '<a type="button" class="btn btn-white btn-xs" data-toggle="modal" data-target="#myModal"><i class="fa fa-wrench"></i></a>';
+                            }
+                        }]
+
+                });
+            });
 
         </script>
 
-        <script>
-            /*
-             $(document).ready(function() {
-             setTimeout(function() {
-             toastr.options = {
-             closeButton: true,
-             progressBar: true,
-             showMethod: 'slideDown',
-             timeOut: 4000
-             };
-             toastr.success('Responsive Admin Theme', 'Welcome to INSPINIA');
-             
-             }, 1300);
-             
-             
-             var data1 = [
-             [0,4],[1,8],[2,5],[3,10],[4,4],[5,16],[6,5],[7,11],[8,6],[9,11],[10,30],[11,10],[12,13],[13,4],[14,3],[15,3],[16,6]
-             ];
-             var data2 = [
-             [0,1],[1,0],[2,2],[3,0],[4,1],[5,3],[6,1],[7,5],[8,2],[9,3],[10,2],[11,1],[12,0],[13,2],[14,8],[15,0],[16,0]
-             ];
-             $("#flot-dashboard-chart").length && $.plot($("#flot-dashboard-chart"), [
-             data1, data2
-             ],
-             {
-             series: {
-             lines: {
-             show: false,
-             fill: true
-             },
-             splines: {
-             show: true,
-             tension: 0.4,
-             lineWidth: 1,
-             fill: 0.4
-             },
-             points: {
-             radius: 0,
-             show: true
-             },
-             shadowSize: 2
-             },
-             grid: {
-             hoverable: true,
-             clickable: true,
-             tickColor: "#d5d5d5",
-             borderWidth: 1,
-             color: '#d5d5d5'
-             },
-             colors: ["#1ab394", "#1C84C6"],
-             xaxis:{
-             },
-             yaxis: {
-             ticks: 4
-             },
-             tooltip: false
-             }
-             );
-             
-             var doughnutData = {
-             labels: ["App","Software","Laptop" ],
-             datasets: [{
-             data: [300,50,100],
-             backgroundColor: ["#a3e1d4","#dedede","#9CC3DA"]
-             }]
-             } ;
-             
-             
-             var doughnutOptions = {
-             responsive: false,
-             legend: {
-             display: false
-             }
-             };
-             
-             
-             var ctx4 = document.getElementById("doughnutChart").getContext("2d");
-             new Chart(ctx4, {type: 'doughnut', data: doughnutData, options:doughnutOptions});
-             
-             var doughnutData = {
-             labels: ["App","Software","Laptop" ],
-             datasets: [{
-             data: [70,27,85],
-             backgroundColor: ["#a3e1d4","#dedede","#9CC3DA"]
-             }]
-             } ;
-             
-             
-             var doughnutOptions = {
-             responsive: false,
-             legend: {
-             display: false
-             }
-             };
-             
-             
-             var ctx4 = document.getElementById("doughnutChart2").getContext("2d");
-             new Chart(ctx4, {type: 'doughnut', data: doughnutData, options:doughnutOptions});
-             
-             });
-             */
-        </script>
+     
     </body>
 </html>
