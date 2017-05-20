42e61f8d3f9ea3eb6bf6ceb21ecfc8587133d8dd
z10188 <z10188@10.0.1.179>
2017-05-20 20:36:45 +0400

    add provider

--- api/addProvider.php
+++ api/addProvider.php
@@ -7,21 +7,14 @@ $dbName = "m3z8z9h6_ghafoman";
 
 $provider_name = $_POST['provider_name'];
 $fileName = $_FILES['provider_logo']['name'];
+$fileName = uniqid().$fileName;
 $tempNAme = $_FILES['provider_logo']['tmp_name'];
 move_uploaded_file($tempNAme, "../uploads/".$fileName);
+$logo_url = "http://admin.ghafoman.net/uploads/"+$fileName;
         
 
 
 
-/*
- * 
- * 
- * 
- * 
- * 
- * 
- * Array ( [___] => Array ( [name] => 3.png [type] => image/png [tmp_name] => /Applications/XAMPP/xamppfiles/temp/phpfX9qsR [error] => 0 [size] => 7511 ) )
-// Create connection
 
 
 $link = mysqli_connect($servername, $username, $password, $dbName);
@@ -29,9 +22,10 @@ $query = "INSERT"
         . " INTO Provider"
         . " (provider_id, "
         . "provider_name,"
-        . "VALUES (NULL, '$provider_name')";
+        ."provider_logo)"
+        . "VALUES (NULL, '$provider_name','$logo_url')";
 
 mysqli_query($link, $query);
 if ($link != null) {
     mysqli_close($link);
-}*/
+}
