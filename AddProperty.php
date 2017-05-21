5404f7561b7546b46c1c0670cc0c069811af4f69
mohammedomar <mohammedomar@10.20.13.119>
2017-05-21 18:05:26 +0400

    provider renter and owner

--- AddProperty.php
+++ AddProperty.php
@@ -28,6 +28,7 @@ if (!isset($_SESSION['loggedin']))
         <link href="css/plugins/dropzone/dropzone.css" rel="stylesheet">
         <link href="css/plugins/jasny/jasny-bootstrap.min.css" rel="stylesheet">
         <link href="css/plugins/codemirror/codemirror.css" rel="stylesheet">
+        <link href="css/plugins/sweetalert/sweetalert.css" rel="stylesheet">
 
         <link href="css/plugins/awesome-bootstrap-checkbox/awesome-bootstrap-checkbox.css" rel="stylesheet">
 
@@ -90,7 +91,7 @@ if (!isset($_SESSION['loggedin']))
 
                             <div class="form-group">
                                 <label class="col-sm-2 control-label">Property Price</label>
-                                <div class="col-sm-10"><input required="" name="propertyPrice" placeholder="e.g., 18000" type="number" class="form-control"></div>
+                                <div class="col-sm-10"><input required="" name="propertyPrice" placeholder="e.g., 18000, the price is per year for rental" type="number" class="form-control"></div>
                             </div>
 
                             <div class="hr-line-dashed"></div>
@@ -222,7 +223,7 @@ if (!isset($_SESSION['loggedin']))
 
                                 <label class="col-sm-2 control-label">Property Pictures</label>
                                 <div class="col-sm-10">
-                                    <input name="files[]" type="file" multiple="multiple">
+                                    <input name="propertyImages[]" type="file" multiple="multiple">
                                 </div>
                             </div>
 
@@ -264,6 +265,9 @@ if (!isset($_SESSION['loggedin']))
         <!-- CodeMirror -->
         <script src="js/plugins/codemirror/codemirror.js"></script>
         <script src="js/plugins/codemirror/mode/xml/xml.js"></script>
+        <script src="js/jquery.form.min.js"></script>
+        <script src="js/plugins/sweetalert/sweetalert.min.js"></script>
+
 
         <script>
 
@@ -284,7 +288,7 @@ if (!isset($_SESSION['loggedin']))
                         $.each(data, function (key, value) {
                             $('#propertyTypes')
                                     .append($("<option></option>")
-                                            .attr("value", value.propretyType_id)
+                                            .attr("value", value.propertyType_id)
                                             .text(value.propertyType_name + "/" + value.ar_propertyType_name));
                         });
 
@@ -476,134 +480,31 @@ if (!isset($_SESSION['loggedin']))
                 else
                     $("#addOwner").hide();
             }
-//            $('form').submit(function () {
-//
-//                var formData = new FormData($(this)[0]);
-//
-//                $('#ibox1').children('.ibox-content').toggleClass('sk-loading');
-//                $.ajax({
-//
-//                    type: "POST",
-//                    cache: false,
-//                    url: $(this).attr('action'),
-//                    data: formData,
-//                    success: function (data) {
-//
-//                        $('#ibox1').children('.ibox-content').toggleClass('sk-loading');
-//
-//                    }
-//
-//                });
-//
-//            });
 
 
 
+            $('form').on('submit', function (e) {
+                $('#ibox1').children('.ibox-content').toggleClass('sk-loading');
+
+                e.preventDefault(); // prevent native submit
+                $(this).ajaxSubmit({
+                    //resetForm: true,
+                    success: function () {
+                        $('#ibox1').children('.ibox-content').toggleClass('sk-loading');
+                        swal({
+                            title: "Saved!",
+                            text: "New property has been added!",
+                            type: "success"
+                        });
+                    }
+                }
+
+                );
+
+            });
 
 
         </script>
 
-        <script>
-            /*
-             $(document).ready(function() {
-             setTimeout(function() {
-             toastr.options = {
-             closeButton: true,
-             progressBar: true,
-             showMethod: 'slideDown',
-             timeOut: 4000
-             };
-             toastr.success('Responsive Admin Theme', 'Welcome to INSPINIA');
-             
-             }, 1300);
-             
-             
-             var data1 = [
-             [0,4],[1,8],[2,5],[3,10],[4,4],[5,16],[6,5],[7,11],[8,6],[9,11],[10,30],[11,10],[12,13],[13,4],[14,3],[15,3],[16,6]
-             ];
-             var data2 = [
-             [0,1],[1,0],[2,2],[3,0],[4,1],[5,3],[6,1],[7,5],[8,2],[9,3],[10,2],[11,1],[12,0],[13,2],[14,8],[15,0],[16,0]
-             ];
-             $("#flot-dashboard-chart").length && $.plot($("#flot-dashboard-chart"), [
-             data1, data2
-             ],
-             {
-             series: {
-             lines: {
-             show: false,
-             fill: true
-             },
-             splines: {
-             show: true,
-             tension: 0.4,
-             lineWidth: 1,
-             fill: 0.4
-             },
-             points: {
-             radius: 0,
-             show: true
-             },
-             shadowSize: 2
-             },
-             grid: {
-             hoverable: true,
-             clickable: true,
-             tickColor: "#d5d5d5",
-             borderWidth: 1,
-             color: '#d5d5d5'
-             },
-             colors: ["#1ab394", "#1C84C6"],
-             xaxis:{
-             },
-             yaxis: {
-             ticks: 4
-             },
-             tooltip: false
-             }
-             );
-             
-             var doughnutData = {
-             labels: ["App","Software","Laptop" ],
-             datasets: [{
-             data: [300,50,100],
-             backgroundColor: ["#a3e1d4","#dedede","#9CC3DA"]
-             }]
-             } ;
-             
-             
-             var doughnutOptions = {
-             responsive: false,
-             legend: {
-             display: false
-             }
-             };
-             
-             
-             var ctx4 = document.getElementById("doughnutChart").getContext("2d");
-             new Chart(ctx4, {type: 'doughnut', data: doughnutData, options:doughnutOptions});
-             
-             var doughnutData = {
-             labels: ["App","Software","Laptop" ],
-             datasets: [{
-             data: [70,27,85],
-             backgroundColor: ["#a3e1d4","#dedede","#9CC3DA"]
-             }]
-             } ;
-             
-             
-             var doughnutOptions = {
-             responsive: false,
-             legend: {
-             display: false
-             }
-             };
-             
-             
-             var ctx4 = document.getElementById("doughnutChart2").getContext("2d");
-             new Chart(ctx4, {type: 'doughnut', data: doughnutData, options:doughnutOptions});
-             
-             });
-             */
-        </script>
     </body>
 </html>
--- api/addProperty.php
+++ api/addProperty.php
@@ -12,9 +12,72 @@ $propertyBaths = $_POST['propertyBaths'];
 $propertyArea = $_POST['propertyArea'];
 $propertyDescription = $_POST['propertyDescription'];
 $ar_propertyDescription = $_POST['ar_propertyDescription'];
-$propertyAmenities = $_POST['propertyAmenities'];
+$propertyAmenities = isset($_POST['propertyAmenities'])?$_POST['propertyAmenities']:null;
 $propertyLatitude = $_POST['propertyLatitude'];
 $propertyLongitude = $_POST['propertyLongitude'];
+$propertyProvider = $_POST['propertyProvider'];
+$propertyOwner = isset($_POST['propertyOwner'])?$_POST['propertyOwner']:"NULL";
+$property360Link = $_POST['property360'];
+
+$servername = "108.167.157.196";
+$username = "m3z8z9h6_admin";
+$password = "Intheend13!";
+$dbName = "m3z8z9h6_ghafoman";
+
+
+
+// Create connection
+$link = mysqli_connect($servername, $username, $password,$dbName);
+$query = "INSERT INTO"
+        . " Property "
+        . "("
+        . "property_id,"
+        . "property_price,"
+        . "property_beds,"
+        . "property_baths,"
+        . "property_description,"
+        . "property_area,"
+        . "property_reference,"
+        . "property_type,"
+        . "property_contract,"
+        . "property_owner,"
+        . "property_provider,"
+        . "ar_property_description,"
+        . "property_geolocation,"
+        . "property_city,"
+        . "property_areaLocation,"
+        . "property_country,"
+        . "property_360view"   
+        . ") "
+        . " VALUES "
+        . "("
+        . "NULL,"
+        . "'$propertyPrice',"
+        . "'$propertyBeds',"
+        . "'$propertyBaths',"
+        . "'$propertyDescription',"
+        . "'$propertyArea',"
+        . "'$propertyReference',"
+        . "$propertyType,"
+        . "'$contractType',"
+        . "$propertyOwner,"
+        . "$propertyProvider,"
+        . "'$ar_propertyDescription',"
+        . "'$propertyLatitude,$propertyLongitude',"
+        . "$propertyCity,"
+        . "$propertyAreaLocation,"
+        . "$propertyCountry,"
+        . "'$property360Link'"
+        . ")";
+
+if(!mysqli_query($link, $query))
+{
+    print_r(mysqli_error_list($link));
+    echo "<br>.$query";
+}
+if($link != null)
+mysqli_close($link);
+
 
 
 /* 
